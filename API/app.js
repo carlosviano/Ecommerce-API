2ef00e90273c6d5fb01fdb3437cee35432e6d840
Carlos <carlitosviano@gmail.com>
2022-11-23 20:31:25 +0100

    ejercicio backend api

--- API/app.js
+++ API/app.js
@@ -2,24 +2,56 @@ let express = require("express");
 let cors = require("cors");
 let app = express();
 
+const productoUno = {
+    id: 1,
+    nombre: "Bananas",
+    stock: 120,
+    precio: 10.9,
+};
+const productoDos = {
+    id:2,
+    nombre: "Manzana",
+    stock: 80,
+    precio: 10.8,
+};
+const productos = [productoUno,productoDos];
+
 app.use(cors());
 
 app.get("/", function(request, response){
     response.send("Bienvenido a mi Ecommerce")
 });
 app.get("/productosdestacados", function(request,response){
-    const productoUno = {
+
+    response.send(productos)
+});
+
+app.get("/detalleProducto", function(request,response){
+    const detalleUno = {
+        id:1,
         nombre: "Bananas",
         stock: 120,
-        precio: 10.9,
+        descripcion: "kfewpfwmfpwodw`pdpkdnqidjqbdkdLDkdñqoifj´`"
     };
-    const productoDos = {
+    const detalleDos = {
+        id:2,
         nombre: "Manzana",
-        stock: 80,
-        precio: 10.8,
+        stock: 13,
+        descripcion: "akdnqojdnwmwmfekfnejfnenm"
     };
-    const productos = [productoUno,productoDos];
-    response.send(productos)
+    const detalles = [detalleUno, detalleDos];
+    response.send(detalles)
+});
+
+app.get("/producto/:id", function(request, response){
+const productoId = request.params.id
+
+for(p of productos){
+    if(productoId == p.id){
+        response.send(p)
+    }
+}
+
 });
 
 app.listen(8000, function(){
